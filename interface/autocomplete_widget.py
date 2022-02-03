ff256c1891510df7df0329c5e0e1cc97fa664704
hammadoonsss <hammadoon.issa@sourcesoftsolutions.com>
2022-02-03 13:10:41 +0530

    If-else in autocomplete_widget.py

--- interface/autocomplete_widget.py
+++ interface/autocomplete_widget.py
@@ -28,16 +28,21 @@ class Autocomplete(tk.Entry):
 
     # Show and Remove the Listbox
 
-    if self._var.get() == "":
+    if self._var.get() == "": # Entry Widget is empty
       if self._lb_open:
         self._lb.destroy()
         self._lb_open = False
+      else:
+        print("listbox_open1", self._lb_open)
+
     else:
       if not self._lb_open:
         self._lb = tk.Listbox(height=8)
         self._lb.place(x=self.winfo_x() + self.winfo_width(), y=self.winfo_y() + self.winfo_height() + 10)
 
         self._lb_open = True
+      else:
+        print("listbox_open2", self._lb_open)
       
       symbols_matched = [symbol for symbol in self._symbols if symbol.startswith(self._var.get())]
 
@@ -54,17 +59,23 @@ class Autocomplete(tk.Entry):
       else:
         if self._lb_open:
           self._lb.destroy()
-          self._lb_open =False 
+          self._lb_open =False
+        else:
+          print("listbox_open3", self._lb_open)
 
   def _select(self, event: tk.Event):
+    # Select function - To get Value from Listbox into Entry widget
     if self._lb_open:
       self._var.set(self._lb.get(tk.ACTIVE))
       self._lb.destroy()
       self._lb_open = False
       self.icursor(tk.END)
+    else:
+      print("listbox_open4", self._lb_open)
+
 
   def _up_down(self, event: tk.Event):
-
+    # Up and Down function - To Choose Volue from Listbox
     if self._lb_open:
       if self._lb.curselection() == ():
         index = -1
@@ -78,9 +89,14 @@ class Autocomplete(tk.Entry):
         index = str(index - 1)
         self._lb.selection_set(first=index)
         self._lb.activate(index)
-
       elif index < lb_size - 1 and event.keysym == "Down":
         self._lb.select_clear(first=index)
         index = str(index + 1)
         self._lb.selection_set(first=index)
         self._lb.activate(index)
+      else: 
+        print("NO Up and Down Key")
+        
+    else:
+      print("listbox_open5", self._lb_open)
+
