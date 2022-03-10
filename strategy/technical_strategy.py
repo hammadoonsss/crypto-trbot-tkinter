491fc4566361a00d4392cf930a61b641a9233b41
hammadoonsss <hammadoon.issa@sourcesoftsolutions.com>
2022-03-10 13:16:09 +0530

    Update BB Strategy

--- strategy/technical_strategy.py
+++ strategy/technical_strategy.py
@@ -80,12 +80,13 @@ class TechnicalStrategy(Strategy):
             ma = bbdf['Close'].rolling(bb_period).mean()
             std = bbdf['Close'].rolling(bb_period).std()
 
-            bbdf['Upper_band']=(ma + bb_multiplier * std).round(1)
-            bbdf['Lower_band']=(ma - bb_multiplier * std).round(1)
+            bbdf['Upper_band'] = (ma + bb_multiplier * std).round(1)
+            bbdf['Lower_band'] = (ma - bb_multiplier * std).round(1)
 
             bbdf = bbdf.dropna()
 
-            bbdf['buy_price'], bbdf['sell_price'], bbdf['bb_signal'] = self.implement_bb_strategy(bbdf)
+            bbdf['buy_price'], bbdf['sell_price'], bbdf['bb_signal'] = self.implement_bb_strategy(
+                bbdf)
             # print("buy, sell, bb_signal:", buy_price, sell_price, bb_signal)
             # self.implement_bb_strategy(bbdf)
 
@@ -95,37 +96,30 @@ class TechnicalStrategy(Strategy):
             try:
                 figure = plt.Figure(figsize=(46, 67), dpi=200)
                 ax = figure.add_subplot(111)
-                ax.scatter(bbdf['DateTime'],bbdf['buy_price'], color = 'g', marker = '^', label = 'BUY',)
-                ax.scatter(bbdf['DateTime'],bbdf['sell_price'], color = 'r', marker = 'v', label = 'SELL',)
+
                 chart_type = FigureCanvasTkAgg(figure, self.root)
                 chart_type.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-                scatter3 = FigureCanvasTkAgg(figure, self.root) 
-                scatter3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+                # scatter3 = FigureCanvasTkAgg(figure, self.root)
+                # scatter3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
 
                 cl = bbdf[['Close']].groupby(bbdf['DateTime']).sum()
                 ub = bbdf[['Upper_band']].groupby(bbdf['DateTime']).sum()
                 lb = bbdf[['Lower_band']].groupby(bbdf['DateTime']).sum()
 
                 cl.plot(kind='line', linestyle='-', linewidth=0.5,
-                        ax=ax, color='#322e2f', fontsize=5)
+                        ax=ax, color='#322e2f', fontsize=5, alpha=0.3)
                 ub.plot(kind='line', linestyle='-.', linewidth=0.5,
                         ax=ax, color='#d72631', fontsize=5)
                 lb.plot(kind='line', linestyle='-.', linewidth=0.5,
                         ax=ax, color='#5c3c92', fontsize=5)
-                
+
+                ax.scatter(bbdf['DateTime'], bbdf['buy_price'],
+                           color='g', marker='^', label='BUY',)
+                ax.scatter(bbdf['DateTime'], bbdf['sell_price'],
+                           color='r', marker='v', label='SELL',)
+
                 ax.legend(loc='lower right', fontsize=5)
                 ax.set_title('--Bollinger Band--')
-
-                # ax.legend(loc='lower right', fontsize=5)
-                # ax.set_title('--Bollinger Band--')
-                # figure3 = plt.Figure(figsize=(5,4), dpi=100)
-                # ax3 = figure3.add_subplot(111)
-                # ax3.scatter(df3['Interest_Rate'],df3['Stock_Index_Price'], color = 'g')
-                # scatter3 = FigureCanvasTkAgg(figure3, root) 
-                # scatter3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-                # ax3.legend(['Stock_Index_Price']) 
-                # ax3.set_xlabel('Interest Rate')
-                # ax3.set_title('Interest Rate Vs. Stock Index Price')
 
                 # bbdf['Close'].plot(label = 'CLOSE PRICES', alpha = 0.3)
                 # bbdf['Upper_band'].plot(label = 'UPPER BB', linestyle = '--', linewidth = 1, color = 'red')
@@ -207,10 +201,10 @@ class TechnicalStrategy(Strategy):
             minus_dm = np.where((downmove > upmove) &
                                 (downmove > 0), downmove, 0.0)
 
-            adx_df['plus_di'] = 100 * (plus_dm/atr['ATR']).ewm(alpha=1/lookback, \
-                                                        min_periods=lookback).mean()
-            adx_df['minus_di'] = 100 * (minus_dm/atr['ATR']).ewm(alpha=1/lookback, \
-                                                        min_periods=lookback).mean()
+            adx_df['plus_di'] = 100 * (plus_dm/atr['ATR']).ewm(alpha=1/lookback,
+                                                               min_periods=lookback).mean()
+            adx_df['minus_di'] = 100 * (minus_dm/atr['ATR']).ewm(alpha=1/lookback,
+                                                                 min_periods=lookback).mean()
 
             pmm = adx_df['plus_di'] - adx_df['minus_di']
             ppm = adx_df['plus_di'] + adx_df['minus_di']
@@ -330,7 +324,7 @@ class TechnicalStrategy(Strategy):
             print("Error ATR-Renko :", e)
 
         try:
-            # ----some error occur in Renko!!---- 
+            # ----some error occur in Renko!!----
             ren_df = df.copy()
             print('ren_df1:________\n', ren_df)
             # ren_df.reset_index(inplace=True)         #-- Currenty NO
@@ -954,25 +948,20 @@ class TechnicalStrategy(Strategy):
                 self._open_position(signal_result)
 
     def implement_bb_strategy(self, data):
-        buy_price=[]
-        sell_price=[]
-        bb_signal=[]
-        signal=0
-        print('inside : ibbs__________' )
-
-        data = data
+        buy_price = []
+        sell_price = []
+        bb_signal = []
+        signal = 0
 
         close = data['Close']
-        print('close: ', type(close))
         upper_bb = data['Upper_band']
         lower_bb = data['Lower_band']
 
         try:
             for i in range(len(close)):
-                # print("-------------------------",close[i])
+
                 if close.iloc[i-1] > lower_bb.iloc[i-1] and close.iloc[i] < lower_bb.iloc[i]:
 
-                    print("inside 1st if__________")
                     if signal != 1:
                         buy_price.append(close.iloc[i])
                         sell_price.append(np.nan)
@@ -985,7 +974,6 @@ class TechnicalStrategy(Strategy):
                         bb_signal.append(0)
 
                 elif close.iloc[i-1] < upper_bb.iloc[i-1] and close.iloc[i] > upper_bb.iloc[i]:
-                    print("inside 1st else__________")
 
                     if signal != -1:
                         buy_price.append(np.nan)
@@ -1004,4 +992,3 @@ class TechnicalStrategy(Strategy):
             print('Error in ibbs: ', e)
 
         return buy_price, sell_price, bb_signal
-        
